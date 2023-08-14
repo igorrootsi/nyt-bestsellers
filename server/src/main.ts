2745c767b2b8feb389ca71750bf4ab848d337c91
Igor Rootsi <igor.rootsi@gmail.com>
2023-08-14 18:52:18 +0300

    Add swagger documentation

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,24 @@ import  "dotenv/config";
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config  } from "./config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { INestApplication } from "@nestjs/common";
+
+function enableSwaggerDocumentation(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('New York Times best sellers')
+    .setDescription('Api for fetching NYT best sellers')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  enableSwaggerDocumentation(app);
   await app.listen(config.getPort());
 }
 bootstrap();
